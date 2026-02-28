d6f949b148ecea1b66a619e81f8616ea00eedd3d
baokieuv <154108649+baokieuv@users.noreply.github.com>
2026-02-28 21:07:21 +0700

    init project

---
+++ server/smart-bin/src/main/java/com/soict/smart_bin/SmartBinApplication.java
@@ -0,0 +1,13 @@
+package com.soict.smart_bin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartBinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartBinApplication.class, args);
+	}
+
+}
---
+++ server/smart-bin/src/test/java/com/soict/smart_bin/SmartBinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soict.smart_bin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartBinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
